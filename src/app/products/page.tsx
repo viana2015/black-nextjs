84c00d83afd646e413b6cc239f604c317783f89e
Cristiano Viana <cristiano.gvianati@gmail.com>
2023-08-12 12:47:41 -0300

    Initialize Project

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Products() {
+  return (
+    <div>
+      <h1> Products</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam ducimus minima nobis
+        necessitatibus corporis harum quas. Saepe accusamus praesentium voluptatem. Quae at
+        distinctio in ipsam dolor ut voluptatum esse magni!
+      </p>
+    </div>
+  );
+}
